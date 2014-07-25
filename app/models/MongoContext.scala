62d33f90b2eadc3fd893222fd5057fba02b3b9ec
joscha <zippelman@web.de>
2014-07-25 23:29:51 +0200

    added db configuration support via application.conf

--- app/models/MongoContext.scala
+++ app/models/MongoContext.scala
@@ -1,28 +1,24 @@
 package models
 
 import play.api.PlayException
-import reactivemongo.api.MongoDriver
-import reactivemongo.api.DB
+import reactivemongo.api.{DB, MongoDriver}
 import reactivemongo.core.nodeset.Authenticate
-import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.Play.current
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object MongoContext {
-//  val dbName = current.configuration.getString("mongodb.default.db")
-//    .getOrElse(throw new PlayException("Configuration error", "Could not find mongodb.default.db in settings"))
-//  val user = current.configuration.getString("mongodb.default.user")
-//    .getOrElse(throw new PlayException("Configuration error", "Could not find mongodb.default.user in settings"))
-//  val password = current.configuration.getString("mongodb.default.password")
-//    .getOrElse(throw new PlayException("Configuration error", "Could not find mongodb.default.password in settings"))
-//  val host = current.configuration.getString("mongodb.default.host")
-//    .getOrElse(throw new PlayException("Configuration error", "Could not find mongodb.default.host in settings"))
+    val dbName = current.configuration.getString("mongodb.default.db")
+      .getOrElse(throw new PlayException("Configuration error", "Could not find mongodb.default.db in settings"))
+    val user = current.configuration.getString("mongodb.default.user")
+      .getOrElse(throw new PlayException("Configuration error", "Could not find mongodb.default.user in settings"))
+    val password = current.configuration.getString("mongodb.default.password")
+      .getOrElse(throw new PlayException("Configuration error", "Could not find mongodb.default.password in settings"))
+    val host = current.configuration.getString("mongodb.default.host")
+      .getOrElse(throw new PlayException("Configuration error", "Could not find mongodb.default.host in settings"))
 
-//  val driver = new MongoDriver
-//  val credentials = List(Authenticate(dbName, user, password))
-//  val connection = driver.connection(List(host), nbChannelsPerNode = 5, authentications = credentials)
-//  def db(): DB = connection.db(dbName)
-
-  val driver = new MongoDriver
-  val connection = driver.connection(List("localhost"))
-  def db(): DB = connection("timetracker")
+    val driver = new MongoDriver
+    val credentials = List(Authenticate(dbName, user, password))
+    val connection = driver.connection(List(host), nbChannelsPerNode = 5, authentications = credentials)
+    def db(): DB = connection.db(dbName)
 }
